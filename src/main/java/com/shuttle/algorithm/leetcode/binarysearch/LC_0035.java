35e35ee9fccb3be4e2c5f44c522814d168fd129a
Shuttle <Shuttleupup@163.com>
2024-05-21 00:35:50 +0800

    feat: Add leetcode 35.Search Insert Position solution

---
+++ src/main/java/com/shuttle/algorithm/leetcode/binarysearch/LC_0035.java
@@ -0,0 +1,55 @@
+package com.shuttle.algorithm.leetcode.binarysearch;
+
+/**
+ * @author: Shuttle
+ * @description: 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 示例 1:
+ * 输入: nums = [1,3,5,6], target = 5
+ * 输出: 2
+ * 示例 2:
+ * 输入: nums = [1,3,5,6], target = 2
+ * 输出: 1
+ */
+public class LC_0035 {
+
+    /**
+     * 同 leetcode 704
+     * 思路：不断取中间值与目标值进行比较
+     * 时间复杂度：O(log n)
+     * 空间复杂度：O(1)
+     *
+     * @param nums   升序数组
+     * @param target 目标值
+     * @return 返回目标值应该插入数组中的索引值
+     */
+    public int searchInsert(int[] nums, int target) {
+        // 数组为空不能插入
+        if (nums == null) {
+            return -1;
+        }
+        int leftIndex = 0;
+        int rightIndex = nums.length - 1;
+        // length == 0 放在数组首位
+        if (nums.length == 0) {
+            return leftIndex;
+        }
+
+        while (leftIndex <= rightIndex) {
+            int midIndex = leftIndex + (rightIndex - leftIndex) / 2;
+            // 找到直接返回
+            if (nums[midIndex] == target) {
+                return midIndex;
+            }
+            if (nums[midIndex] > target) {
+                // target -> [leftIndex, midIndex - 1]
+                rightIndex = midIndex - 1;
+            } else {
+                // target -> [midIndex + 1, rightIndex]
+                leftIndex = midIndex + 1;
+            }
+        }
+
+        return leftIndex;
+    }
+
+}
