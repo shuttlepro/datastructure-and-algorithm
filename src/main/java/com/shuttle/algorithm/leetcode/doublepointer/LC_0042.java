75ddf163b7997443b0595b11b8e0130b1b9a77c7
Shuttle <Shuttleupup@163.com>
2024-05-24 22:52:11 +0800

    feat: Add leetcode 42.Trapping Rain Water solution

---
+++ src/main/java/com/shuttle/algorithm/leetcode/doublepointer/LC_0042.java
@@ -0,0 +1,46 @@
+package com.shuttle.algorithm.leetcode.doublepointer;
+
+/**
+ * @author: Shuttle
+ * @description: 给定 n 个非负整数表示每个宽度为 1 的柱子的高度图，计算按此排列的柱子，下雨之后能接多少雨水。
+ * 原题出处：<a href="https://leetcode.cn/problems/trapping-rain-water/description">接雨水</a>
+ * 输入：height = [0,1,0,2,1,0,1,3,2,1,2,1]
+ * 输出：6
+ */
+public class LC_0042 {
+
+    /**
+     * 思路：双指针，对于每一个柱子，它能接的水 = min(左右两边最高柱子）- 当前柱子高度
+     *
+     * @param height 代表高度图的整数数组
+     * @return 能接的所有雨水总和
+     */
+    public int trap(int[] height) {
+        if (height == null || height.length < 3) {
+            return 0;
+        }
+        int waterCapacity = 0;
+        int leftIndex = 0;
+        int rightIndex = height.length - 1;
+        // 维护左右两边最高柱子
+        int leftMaxHeight = 0;
+        int rightMaxHeight = 0;
+
+        while (leftIndex < rightIndex) {
+            leftMaxHeight = Math.max(leftMaxHeight, height[leftIndex]);
+            rightMaxHeight = Math.max(rightMaxHeight, height[rightIndex]);
+            // 如果当前左边柱子高度 > 当前右边柱子高度，说明右边可以接水
+            if (height[leftIndex] > height[rightIndex]) {
+                waterCapacity += rightMaxHeight - height[rightIndex];
+                rightIndex--;
+            } else {
+                // 同理
+                waterCapacity += leftMaxHeight - height[leftIndex];
+                leftIndex++;
+            }
+        }
+
+        return waterCapacity;
+    }
+
+}
