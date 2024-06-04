dcaf50df9cb8a70cf3087531203d0136210d17ea
Shuttle <Shuttleupup@163.com>
2024-06-04 21:49:03 +0800

    feat: Add leetcode 509.Fibonacci Number solution

---
+++ src/main/java/com/shuttle/algorithm/leetcode/dynamicprogramming/LC_0509.java
@@ -0,0 +1,53 @@
+package com.shuttle.algorithm.leetcode.dynamicprogramming;
+
+/**
+ * @author: Shuttle
+ * @description: <a href="https://leetcode.cn/problems/fibonacci-number/description">斐波那契数</a>
+ */
+public class LC_0509 {
+
+    /**
+     * 解法一：滚动数组
+     *
+     * @param n 第 n 个
+     * @return 第 n 个斐波那契数
+     */
+    public int fibSolution1(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int prev = 0;
+        int next = 0;
+        int fib = 1;
+
+        for (int i = 2; i <= n; i++) {
+            prev = next;
+            next = fib;
+            fib = prev + next;
+        }
+
+        return fib;
+    }
+
+    /**
+     * 解法二：动态规划
+     *
+     * @param n 第 n 个
+     * @return 第 n 个斐波那契数
+     */
+    public int fibSolution2(int n) {
+        if (n < 2) {
+            return n;
+        }
+        // dp[i] 代表第 i 个斐波那契数
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+
+}
